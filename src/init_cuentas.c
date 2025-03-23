5d32ee084b04a36afb48cb65b92aab2219ec95de
JuanTorres5y60984 <yosoymutuero@gmail.com>
2025-03-23 14:27:15 +0100

    cambios

--- src/init_cuentas.c
+++ src/init_cuentas.c
@@ -1,6 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
 struct Cuenta{
     int numero_cuenta;
     char titular[50];
     float saldo;
     int num_transacciones;
 };
+
+void CrearCuentas(char nombre_usuario[50]){
+    struct Cuenta nueva_cuenta;
+    nueva_cuenta.numero_cuenta = 123456; // Puedes generar un número de cuenta único
+    strcpy(nueva_cuenta.titular, nombre_usuario);
+    nueva_cuenta.saldo = 0.0;
+    nueva_cuenta.num_transacciones = 0;
+
+    printf("Cuenta creada:\n");
+    printf("Numero de cuenta: %d\n", nueva_cuenta.numero_cuenta);
+    printf("Titular: %s\n", nueva_cuenta.titular);
+    printf("Saldo: %.2f\n", nueva_cuenta.saldo);
+    printf("Numero de transacciones: %d\n", nueva_cuenta.num_transacciones);
+}
+
